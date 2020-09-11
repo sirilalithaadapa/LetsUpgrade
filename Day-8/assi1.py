ee904757d8a3a7a1bb4c87d21a69539df40eff39
Siri Lalitha <sirilalitha9088@gmail.com>
2020-09-11 17:35:40 +0530

    Create assi1.py

---
+++ Day-8/assi1.py
@@ -0,0 +1,14 @@
+def smart_divide(func):
+    def inner(a, b):
+		a= int(input("Enter a:"))
+		b= int(input("Enter b:"))
+        print("I am going to divide", a, "and", b)
+        if b == 0:
+            print("OOPs! cannot divide")
+            return
+
+        return func(a, b)
+    return inner
+
+def divide(a, b):
+    print(a/b)
